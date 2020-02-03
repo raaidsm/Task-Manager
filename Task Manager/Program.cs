b1bb0ed13f20bc310a9240bab0a59c41fe15f10d
Raaid Mahboob <raaidsm@gmail.com>
2020-02-03 02:58:42 -0500

    Initial commit

---
+++ Task Manager/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Task_Manager
+{
+    class Program {
+        static void Main(string[] args) {
+            var taskMan = new TaskManager();
+            taskMan.Run(); } }
+
+    class TaskManager {
+        List<Task> taskList = new List<Task>();
+        private class Task {
+            internal string name;
+            public Task(string nameArg) {
+                name = nameArg; } }
+
+        internal void Run() {
+            CreateTask();
+            DisplayTasks(); }
+
+        internal void CreateTask() {
+            Console.WriteLine("Enter names of tasks you wish to create. Type \"Done\" to exit: ");
+            string userInput = Console.ReadLine();
+            while (userInput != "Done") {
+                taskList.Add(new Task(userInput));
+                userInput = Console.ReadLine(); } }
+
+        internal void DisplayTasks() {
+            int length = taskList.Count;
+            foreach (Task task in taskList) {
+                Console.Write(task.name);
+                if (0 < --length) {
+                    Console.Write(", "); } } } }
+}
